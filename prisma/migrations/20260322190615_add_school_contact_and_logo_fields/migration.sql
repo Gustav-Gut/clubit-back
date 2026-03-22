6e5b5f6c98aeda25e1c86bc711a475dad0647d3e
Gustav-Gut <gustavo.gutierrezy@gmail.com>
2026-03-22 20:07:15 -0300

    [ADD] New fields to school details info

---
+++ prisma/migrations/20260322190615_add_school_contact_and_logo_fields/migration.sql
@@ -0,0 +1,6 @@
+-- AlterTable
+ALTER TABLE "School" ADD COLUMN     "description" TEXT,
+ADD COLUMN     "email" TEXT,
+ADD COLUMN     "logoUrl" TEXT,
+ADD COLUMN     "phone" TEXT,
+ADD COLUMN     "website" TEXT;
